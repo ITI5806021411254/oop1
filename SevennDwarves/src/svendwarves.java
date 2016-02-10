f497630f9c9bb5d2928d2fb3d86c1e8b82af4ebe
KungII <KungII@Kung>
2016-02-10 23:29:27 +0700

    Test SevenDwarves

---
+++ SevennDwarves/src/svendwarves.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+public class svendwarves {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		    int n1,n2,n3,n4,n5,n6,n7,n8,n9;
+		    		
+		    		Scanner scan =new Scanner(System.in);
+		    		System.out.println("Enter number 1 :: ");
+		            n1 = scan.nextInt();
+		            System.out.println("Enter number 2 :: ");
+		            n2 = scan.nextInt();
+		            System.out.println("Enter number 3 :: ");
+		            n3 = scan.nextInt();
+		            System.out.println("Enter number 4 :: ");
+		            n4 = scan.nextInt();
+		            System.out.println("Enter number 5 :: ");
+		            n5 = scan.nextInt();
+		            System.out.println("Enter number 6 :: ");
+		            n6 = scan.nextInt();
+		            System.out.println("Enter number 7 :: ");
+		            n7 = scan.nextInt();
+		            System.out.println("Enter number 8 :: ");
+		            n8 = scan.nextInt();
+		            System.out.println("Enter number 9 :: ");
+		            n9 = scan.nextInt();
+		      //n1-n9      
+		            if ((n3+n4+n5+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n1 = " +n1 +", " +"n2 = "+n2 +" " +"No Dwerves");
+		            if ((n2+n4+n5+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n1 = " +n1 +", " +"n3 = "+n3 +" " +"No Dwerves");
+		            if ((n2+n3+n5+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n1 = " +n1 +", " +"n4 = "+n4 +" " +"No Dwerves");
+		            if ((n2+n3+n4+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n1 = " +n1 +", " +"n5 = "+n5 +"    " +"No Dwerves");
+		            if ((n2+n3+n4+n5+n7+n8+n9) ==100)
+		            	System.out.println(" n1 = " +n1 +", " +"n6 = "+n6 +"    " +"No Dwerves");
+		            if ((n2+n3+n4+n5+n6+n8+n9) ==100)
+		            	System.out.println(" n1 = " +n1 +", " +"n7 = "+n7 +"    " +"No Dwerves");
+		            if ((n2+n3+n4+n5+n6+n7+n9) ==100)
+		            	System.out.println(" n1 = " +n1 +", " +"n8 = "+n8 +"    " +"No Dwerves");
+		            if ((n2+n3+n4+n5+n6+n7+n8) ==100)
+		            	System.out.println(" n1 = " +n1 +", " +"n9 = "+n9 +"    " +"No Dwerves");
+		       //n2-n9
+		            if ((n1+n4+n5+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n2 = " +n2 +", " +"n3 = "+n3 +"    " +"No Dwerves");
+		            if ((n1+n3+n5+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n2 = " +n2 +", " +"n4 = "+n4 +"    " +"No Dwerves");
+		            if ((n1+n3+n4+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n2 = " +n2 +", " +"n5 = "+n5 +"    " +"No Dwerves");
+		            if ((n1+n3+n4+n5+n7+n8+n9) ==100)
+		            	System.out.println(" n2 = " +n2 +", " +"n6 = "+n6 +"    " +"No Dwerves");
+		            if ((n1+n3+n4+n5+n6+n8+n9) ==100)
+		            	System.out.println(" n2 = " +n2 +", " +"n7 = "+n7 +"    " +"No Dwerves");
+		            if ((n1+n3+n4+n5+n6+n7+n9) ==100)
+		            	System.out.println(" n2 = " +n2 +", " +"n8 = "+n8 +"    " +"No Dwerves");
+		            if ((n1+n3+n4+n5+n6+n7+n8) ==100)
+		            	System.out.println(" n2 = " +n2 +", " +"n9 = "+n9 +"    " +"No Dwerves");
+		      //n3-n9
+		            if ((n1+n2+n5+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n3 = " +n3 +", " +"n4 = "+n4 +"    " +"No Dwerves");
+		            if ((n1+n2+n4+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n3 = " +n3 +", " +"n5 = "+n5 +"    " +"No Dwerves");
+		            if ((n1+n2+n4+n5+n7+n8+n9) ==100)
+		            	System.out.println(" n3 = " +n3 +", " +"n6 = "+n6 +"    " +"No Dwerves");
+		            if ((n1+n2+n4+n5+n6+n8+n9) ==100)
+		            	System.out.println(" n3 = " +n3 +", " +"n7 = "+n7 +"    " +"No Dwerves");
+		            if ((n1+n2+n4+n5+n6+n7+n9) ==100)
+		            	System.out.println(" n3 = " +n3 +", " +"n8 = "+n8 +"    " +"No Dwerves");
+		            if ((n1+n2+n4+n5+n6+n7+n8) ==100)
+		            	System.out.println(" n3 = " +n3 +", " +"n9 = "+n9 +"    " +"No Dwerves");
+		      //n4-n9
+		          
+		            if ((n1+n2+n3+n6+n7+n8+n9) ==100)
+		            	System.out.println(" n4 = " +n4 +", " +"n5 = "+n5 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n5+n7+n8+n9) ==100)
+		            	System.out.println(" n4 = " +n4 +", " +"n6 = "+n6 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n5+n6+n8+n9) ==100)
+		            	System.out.println(" n4 = " +n4 +", " +"n7 = "+n7 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n5+n6+n7+n9) ==100)
+		            	System.out.println(" n4 = " +n4 +", " +"n8 = "+n8 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n5+n6+n7+n8) ==100) 
+		            	System.out.println(" n4 = " +n4 +", " +"n9 = "+n9 +"    " +"No Dwerves");
+		      //n5-n9
+		            if ((n1+n2+n3+n4+n7+n8+n9) ==100)
+		            	System.out.println(" n5 = " +n5 +", " +"n6 = "+n6 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n4+n6+n8+n9) ==100)
+		            	System.out.println(" n5 = " +n5 +", " +"n7 = "+n7 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n4+n6+n7+n9) ==100) 
+		            	System.out.println(" n5 = " +n5 +", " +"n8 = "+n8 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n4+n6+n7+n8) ==100)
+		            	System.out.println(" n5 = " +n5 +", " +"n9 = "+n9 +"    " +"No Dwerves");
+		     //n6-n9
+		            if ((n1+n2+n3+n4+n5+n8+n9) ==100)
+		            	System.out.println(" n6 = " +n6 +", " +"n7 = "+n7 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n4+n5+n7+n9) ==100)
+		            	System.out.println(" n6 = " +n6 +", " +"n8 = "+n8 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n4+n5+n7+n8) ==100)
+		            	System.out.println(" n6 = " +n6 +", " +"n9 = "+n9 +"    " +"No Dwerves");
+
+		     //n7-n9
+		            if ((n1+n2+n3+n4+n5+n6+n9) ==100)
+		            	System.out.println(" n7 = " +n7 +", " +"n8 = "+n8 +"    " +"No Dwerves");
+		            if ((n1+n2+n3+n4+n5+n6+n8) ==100)
+		            	System.out.println(" n7 = " +n7 +", " +"n9 = "+n9 +"    " +"No Dwerves");
+
+		     //n8-n9
+		            if ((n1+n2+n3+n4+n5+n6+n7) ==100)
+		            	System.out.println(" n8 = " +n8 +", " +"n9 = "+n9 +"    " +"No Dwerves");
+			}
+
+
+
+
+	}
+
+
